07ab4781d5fd47ff93b635391508b70dfa0d15e1
Geraldo Vilela <geraldovilela19@gmail.com>
2020-06-15 09:43:57 -0400

    start da pagina de logon

--- front-end/src/pages/Logon/index.js
+++ front-end/src/pages/Logon/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
-
+import socialFb from '../../Components/ButtonLogon/Facebook';
+import socialGs from '../../Components/ButtonLogon/Google';
 
 function Logon() {
     return (
         
-        <div >
+        <div id='container'>
+            <div id='logon'>
             <h1>Logon</h1>
+            <span>Email:</span>
+            <input type="email" name="email" id="email"/>
+            <span>Senha:</span>
+            <input type="text" name="password" id="password"/>
+
+            <button type="submit">Logar</button>
+            </div>
+
+            <div className="social-loguin">
+                <socialFb></socialFb>
+                <socialGs></socialGs>
+            </div>
         </div>
 
 
