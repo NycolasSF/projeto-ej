9088dc3f09a576d9628a919098e6dca8cda3587d
Geraldo Vilela <geraldovilela19@gmail.com>
2020-06-15 20:50:54 -0400

    inicio page de login e css teste

--- front-end/src/pages/Logon/index.js
+++ front-end/src/pages/Logon/index.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import socialFb from '../../Components/ButtonLogon/Facebook';
-import socialGs from '../../Components/ButtonLogon/Google';
+import socialFb from '../../Components/ButtonLogon/Facebook/index';
+import socialGs from '../../Components/ButtonLogon/Google/index';
+import './style.css';
 
 function Logon() {
     return (
-        
-        <div id='container'>
-            <div id='logon'>
-            <h1>Logon</h1>
-            <span>Email:</span>
-            <input type="email" name="email" id="email"/>
-            <span>Senha:</span>
-            <input type="text" name="password" id="password"/>
 
-            <button type="submit">Logar</button>
+        <div id='container' className='container'>
+            <div id='logon'>
+                <div className="inputgroup">
+                    <h1>Logon</h1>
+                    <span>Email:</span>
+                    <input type="email" name="email" id="email" />
+                    <span>Senha:</span>
+                    <input type="text" name="password" id="password" />
+
+                    <button type="submit">Logar</button>
+                </div>
             </div>
 
             <div className="social-loguin">
