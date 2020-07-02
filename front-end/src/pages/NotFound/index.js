763684d5f75273b81c9890eb32a915ee251dfd9c
= <=>
2020-07-02 01:42:51 -0400

    Ajustando página NotFound

--- front-end/src/pages/NotFound/index.js
+++ front-end/src/pages/NotFound/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+
+import {FaArrowLeft} from 'react-icons/fa';
+
 import './style.css'
-import noFound from '../../assets/404.svg'
+import noFound from '../../assets/404.svg';
 
 function Register() {
+
+
     return (
         
         <div className="div_nofound">
-            <h1 className="nofound_title">Página não encontrada</h1>
+            <div className="content-title">
+                <Link className="btn-goBack" to='/' >
+                    <FaArrowLeft size={25} className="icon-arrowLeft" />
+                </Link>
+                <h1 className="nofound_title">Página não encontrada</h1>
+            </div>
             <img src={noFound} alt="404"  className="nofound_image"/>
         </div>
 
